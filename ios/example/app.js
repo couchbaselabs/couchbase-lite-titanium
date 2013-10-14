c18bb483f67871abec3204338e7c187d34646f99
Wayne Carter <wayne.carter@couchbase.com>
2013-10-13 21:57:08 -0700

    Update to a simpler self contained example that will work out of the box.

--- ios/example/app.js
+++ ios/example/app.js
@@ -4,104 +4,139 @@
 // to users on how to use it by example.
 
 
-// open a single window
+// Open a single window.
 var win = Ti.UI.createWindow({
-    backgroundColor:'white'
+    title:'Food',
+    backgroundColor:'white',
+    fullscreen:true
 });
-var label = Ti.UI.createLabel();
-win.add(label);
+
+// Add a list view to the window.
+var listView = Titanium.UI.createListView();
+win.add(listView);
+
+// Show the window.
 win.open();
 
-var SYNC_URL = "http://checkers.sync.couchbasecloud.com/checkers";
+// TODO: To sync w/ a server database add your Sync Gateway
+// url.  You can get a free developer sandbox from
+// http://www.couchbasecloud.com
+var SYNC_URL = null;
 
 var CBLite = require('com.couchbase.cbl');
 var manager = CBLite.createManager();
-var database = manager.createDatabase("checkers");
+var database = manager.createDatabase('food');
 
-// Listen for Database changes.
-label.text = "Documents: " + database.queryAllDocuments.rows.count;
-database.addEventListener(database.CHANGE_EVENT, function(e) {
-    var query = e.source.queryAllDocuments;
-    label.text = "Documents: " + query.rows.count;
-});
-
-// Get/Create unique user id.
-var userId = Ti.App.Properties.getString('userId');
-if (!userId) {
-    userId = Titanium.Platform.createUUID();
-    Ti.App.Properties.setString('userId', userId);
-}
-
-// Build document ids.
-var userDocId = "user:" + userId;
-var voteDocId = "vote:" + userId;
-
-// Replicators
-//
-// BUG: There are issues w/ replications during cold-start.
-// https://github.com/couchbase/couchbase-lite-ios/issues/150
-// 
-// Setting isPersistent=true causes the most issues.To work
-// around this you can remove replication config (i.e. replicate
-// everything) or run a few times until the exceptions stop.
-var replications = database.replicate(SYNC_URL, true);
-for (var i=0; i<replications.length; i++) {
-    var replication = replications[i];
+// If there is a sync URL configured then set up replication.
+if (SYNC_URL) {
+    // BUG: There are issues w/ replications during cold-start.
+    // Setting isPersistent=true causes the most issues. To work
+    // around this you can remove replication config (i.e. replicate
+    // everything) or run a few times until the exceptions stop.
+    //
+    // https://github.com/couchbase/couchbase-lite-ios/issues/150
     
-    replication.isContinuous = true;
-    replication.isPersistent = true;
+    var replications = database.replicate(SYNC_URL, true);
     
-    if(replication.isPull) {
-        replication.filter = "sync_gateway/bychannel";
-        replication.queryParams = {"channels":"game"};
-    } else {
-        database.defineFilter("pushItems", function(properties, params) {
-            return (properties._id == userDocId || properties._id == voteDocId);
-        });
+    for (var i=0; i<replications.length; i++) {
+        var replication = replications[i];
         
-        replication.filter = "pushItems";
+        replication.isContinuous = true;
     }
 }
 
-// Update Team doc.
-var userDoc = database.getDocument(userDocId);
-if (!userDoc.currentRevision) userDoc.putProperties({});
-userDoc.update(function(newRevision) {
-    newRevision.putProperties({
-        "team":0
-    });
-    
-    return true;
+// Create view for food by type.
+var foodByType = database.getView("foodByType");
+foodByType.setMap(function(doc, emitter) {
+    if (doc['_id'].indexOf('food:') == 0) {
+        Ti.API.info('CBL: foodByType.emit(): ' + doc['_id']);
+        
+        emitter.emit([doc.type, doc.name], doc);
+    }
+}, "1.0");
+
+// Create/observe live query for food.
+var foodQuery = foodByType.createQuery();
+var foodLiveQuery = foodQuery.toLiveQuery();
+foodLiveQuery.start();
+foodLiveQuery.addEventListener(foodLiveQuery.CHANGE_EVENT, function(e) {
+    // BUG: There are issues w/ queries returning a row enumerator that
+    // does not enumerate correctly.  For now we will just use all the
+    // docs but usually this would be the event source's rows
+    // i.e. showFood(e.source.rows);
+    //
+    // https://github.com/couchbase/couchbase-lite-ios/issues/157
+    //
+    showFood(database.queryAllDocuments.rows);
 });
 
-// Update Vote doc.
-var voteDoc = database.getDocument(voteDocId);
-if (!voteDoc.currentRevision) voteDoc.putProperties({});
-voteDoc.update(function(newRevision) {
-    newRevision.putProperties({
-        "game":1,
-        "turn":1,
-        "team":0,
-        "piece":7,
-        "locations":[11,15]
-    });
+function showFood(rows) {
+    var sections = {};
     
-    return true;
-});
-
-// Create view for games by start time.
-var gamesByStartTime = database.getView("gamesByStartTime");
-if (!gamesByStartTime.map) {
-    gamesByStartTime.setMapAndReduce(function(doc, emitter) {
-        if (doc["_id"].indexOf("game:") == 0 && doc.startTime) {
-            emitter.emit(doc.startTime, doc);
+    var i=1;
+    var row = rows.nextRow;
+    while (row) {
+        Ti.API.info('CBL: Show food[' + i + '/' + rows.count + ']: ' + row.documentId);
+        
+        var properties = row.document.properties;
+        
+        // Get unique section.
+        var sectionTitle = properties.type;
+        var section = sections[sectionTitle];
+        if (!section) {
+            section = Ti.UI.createListSection({
+                headerTitle:sectionTitle
+            });
+            
+            sections[sectionTitle] = section;
         }
-    }, null, "1.0");
+        
+        // Add item to section.
+        section.appendItems([
+            {properties: {title:properties.name}}
+        ]);
+        
+        i++;
+        row = rows.nextRow;
+    }
+    
+    var sectionsArray = [];
+    for (var section in sections) {
+        sectionsArray.push(sections[section]);
+    }
+    
+    listView.sections = sectionsArray;
 }
-// Create/observe live query for the latest game.
-var liveQuery = gamesByStartTime.createQuery().toLiveQuery();
-liveQuery.limit = 1;
-liveQuery.descending = true;
-liveQuery.addEventListener(liveQuery.CHANGE_EVENT, function(e) {
-    Ti.API.info("### liveQuery.change: " + e.source + ", " + e.source.rows + "[" + e.source.rows.count + "]");
-});
+
+// Init data on first run.
+if (database.queryAllDocuments.rows.count == 0) {
+    Ti.API.info('CBL: Init database');
+    
+    var foods = [
+        ['Fruit','Apple'],
+        ['Fruit','Banana'],
+        ['Vegetable','Carrot'],
+        ['Vegetable','Potatoe'],
+        ['Meat', 'Beef'],
+        ['Meat', 'Chicken'],
+        ['Fish','Cod'],
+        ['Fish','Haddock']
+    ];
+    
+    for (var i=0; i<foods.length; i++) {
+        var food = foods[i];
+        var id = 'food:' + food[1];
+        
+        var foodDoc = database.getDocument(id);
+        foodDoc.update(function(newRevision) {
+            Ti.API.info('CBL: Add document: ' + newRevision.document.documentId);
+            
+            newRevision.putProperties({
+                'type':food[0],
+                'name':food[1]
+            });
+            
+            return true;
+        });
+    }
+}
